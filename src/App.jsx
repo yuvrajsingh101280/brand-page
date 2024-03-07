e15659ea11af3f948f65520dbecdbe81a20aac5d
yuvrajsingh101280 <singhyuvraj65035@gmail.com>
2024-03-07 10:32:31 +0530

    brand page

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+export default function App() {
+  return (
+    <>
+      <div>
+        <Navbar />
+        <Hero />
+      </div>
+    </>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="container">
+                <div className="logo">
+                    <img src="/images/brand_logo.png" alt="logo" />
+                </div>
+                <ul>
+                    <li href="#" >Menu</li>
+                    <li href="#" >Location</li>
+                    <li href="#" >About</li>
+                    <li href="#" >Contact</li>
+                </ul>
+
+                <button>Login</button>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
